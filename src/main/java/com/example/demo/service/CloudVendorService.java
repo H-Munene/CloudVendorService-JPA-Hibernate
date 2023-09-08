9c3381d75222f6d32e16ee0631e75713fad34bce
Hezekiah Munene <hezemunene01@gmail.com>
2023-09-08 22:28:41 +0300

    declared service class methods

--- src/main/java/com/example/demo/service/CloudVendorService.java
+++ src/main/java/com/example/demo/service/CloudVendorService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
-public interface CloudVendorService {
+import java.util.List;
 
+import com.example.demo.model.CloudVendor;
+
+public interface CloudVendorService {
+	
+	public String createCloudVendor(CloudVendor cloudVendor) ; //create and add vendor to repo
+	public String updateCloudVendor(CloudVendor cloudvendor) ; // update cloud vendor details
+	public String deleteCloudVendor(String cloudVendorId); //delete a cloud vendor
+	public CloudVendor getCloudVendor(String cloudVendorId); //get one cloud vendor
+	public List<CloudVendor> getAllCloudVendor(); //get all vendors in repo
+	
 }
