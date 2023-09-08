7f17d8bc5a8ff8403f1065b6703346fe6e8a34e3
Hezekiah Munene <hezemunene01@gmail.com>
2023-09-08 23:24:16 +0300

    implemented service layer methods

---
+++ src/main/java/com/example/demo/service/CloudVendorServiceImplementation.java
@@ -0,0 +1,67 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.CloudVendor;
+import com.example.demo.repository.CloudVendorRepository;
+
+@Service 
+/*
+ * mandatory annotation for the Service layer
+ */
+public class CloudVendorServiceImplementation implements CloudVendorService{
+
+	CloudVendorRepository  cloudVendorepo;
+	/*
+	 * need this instance since its the intermediate layer
+	 * to the actual database
+	 */
+	
+	public CloudVendorServiceImplementation(CloudVendorRepository cloudVendorepo) {
+		super();
+		this.cloudVendorepo = cloudVendorepo;
+	}
+	
+	
+	//overridden methods
+	
+	@Override
+	public String createCloudVendor(CloudVendor cloudVendor) {
+		
+		cloudVendorepo.save(cloudVendor); //saves data to db
+		
+		return "Successful insertion";
+	}
+
+	@Override
+	public String updateCloudVendor(CloudVendor cloudvendor) {
+		
+		cloudVendorepo.save(cloudvendor); //saves updated data to db
+		
+		return "Updated Successfully";
+	}
+
+	@Override
+	public String deleteCloudVendor(String cloudVendorId) {
+		
+		cloudVendorepo.deleteById(cloudVendorId);
+		
+		return "Successful deletion";
+	}
+
+	//
+	@Override
+	public CloudVendor getCloudVendor(String cloudVendorId) {
+		
+		return cloudVendorepo.findById(cloudVendorId).get();
+	}
+
+	@Override
+	public List<CloudVendor> getAllCloudVendor() {
+		
+		return cloudVendorepo.findAll();
+	}
+
+}
